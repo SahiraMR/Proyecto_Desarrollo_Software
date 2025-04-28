c267a5d2c29d40c4a95b472fba455d4152ff21bb
SahiraMR <samedina91@ucatolica.edu.co>
2025-04-28 18:49:41 -0500

    agregar comentario crud

--- crud/comentarios_crud.py
+++ crud/comentarios_crud.py
@@ -26,3 +26,12 @@ def escribir_csv(comentarios: List[dict]):
 
 def listar_comentarios():
     return leer_csv()
+
+def agregar_comentario(comentario: Comentario):
+    comentarios = leer_csv()
+    if not (1 <= comentario.calificacion <= 5):
+        raise HTTPException(status_code=400, detail="La calificación debe estar entre 1 y 5.")
+    comentario.id = max([int(c["id"]) for c in comentarios], default=0) + 1
+    comentarios.append(comentario.dict())
+    escribir_csv(comentarios)
+    return comentario
